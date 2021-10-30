66285b21f2113db62163fdedc90a0ea835cfd2a7
Dmitriy Dudnik <wed2288@gmail.com>
2021-10-30 21:56:37 +0300

    Добавляет JS 1 задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup = galleryItems
+  .map(
+    item =>
+      `<a class="gallery__item" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+          />
+        </a>`,
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
 
 console.log(galleryItems);
