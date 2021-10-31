146c14c2d33a24a81bf0689d340ee8d937acb7c0
Dmitriy Dudnik <wed2288@gmail.com>
2021-10-31 23:51:45 +0200

    Добавляет JS для 2 и 3 задания

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const player = new Player('vimeo-player', {
+  id: 236203659,
+  width: 640,
+});
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(event) {
+  localStorage.setItem('videoplayer-current-time', `${event.seconds}`);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const feedbackFormEl = document.querySelector('.feedback-form');
+
+initForm();
+
+feedbackFormEl.addEventListener('input', event => {
+  event.preventDefault;
+  const formData = new FormData(feedbackFormEl);
+});
+
+function onfeedbackForm(event) {
+  let persistedForm = localStorage.getItem(LOCALSTORAGE_KEY);
+  persistedForm = persistedForm ? JSON.parse(persistedForm) : {};
+  persistedForm[event.target.name] = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(persistedForm));
+  console.log(persistedForm);
+}
+
+feedbackFormEl.addEventListener('input', throttle(onfeedbackForm, 500));
+
+feedbackFormEl.addEventListener('submit', () => {
+  console.log('feedback-form-state', JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+});
+
+function initForm() {
+  let persistedForm = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (persistedForm) {
+    persistedForm = JSON.parse(persistedForm);
+    Object.entries(persistedForm).forEach(([name, value]) => {
+      feedbackFormEl.elements[name].value = value;
+    });
+  }
+}
